18d5cfcfbdce943d5f3bd09420484e1e1c9857a7
Dasein1998 <dasein1998@gmail.com>
2023-10-15 09:47:29 +0800

    dirvish

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 depends:
 1. librime
 2. rg
+3. Windows: 'scoop install coreutils fd poppler imagemagick unzip'
+4. pacman -S fd poppler ffmpegthumbnailer mediainfo imagemagick tar unzip
 
 ## librime
 for windows
@@ -13,14 +15,14 @@ scoop install librime
 scoop install ripgrep
 ```
 
-for arch
+for Arch
 
 ``` shell
 sudo pacman -S librime
 ```
 
 
-# reference
+# Reference
 [专业 Emacs 入门教程 - 知乎](https://www.zhihu.com/column/c_1440829147212279808)
 
 [Emacs 自力求生指南 ── 来写自己的配置吧 - Nayuki's Archive](https://nyk.ma/posts/emacs-write-your-own/#%E8%BE%93%E5%85%A5%E6%B3%95)
@@ -30,5 +32,3 @@ sudo pacman -S librime
 [Org-mode 简明手册 - open source - 博客园](https://www.cnblogs.com/Open_Source/archive/2011/07/17/2108747.html
 
 [emacs-newbie/introduction-to-builtin-modes.md at master · condy0919/emacs-newbie](https://github.com/condy0919/emacs-newbie/blob/master/introduction-to-builtin-modes.md)
-
-
--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ; (require 'benchmark-init-modes)
 ; (require 'benchmark-init)
 ; (benchmark-init/activate)
-    ;; Emacs configuration file content is written below.
+;; Emacs configuration file content is written below.
 
 )
 ;; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
@@ -15,14 +15,16 @@
 (electric-pair-mode t)                       ; 自动补全括号
 (add-hook 'prog-mode-hook #'show-paren-mode) ; 编程模式下，光标在括号上时高亮另一个括号
 (column-number-mode t)                       ; 在 Mode line 上显示列号
-;(global-auto-revert-mode t)                  ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
+(global-auto-revert-mode t)                  ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
 (delete-selection-mode t)                    ; 选中文本后输入文本会替换文本（更符合我们习惯了的其它编辑器的逻辑）
 (setq make-backup-files nil)                 ; 关闭文件自动备份
 (add-hook 'prog-mode-hook #'hs-minor-mode)   ; 编程模式下，可以折叠代码块
 (global-display-line-numbers-mode 1)         ; 在 Window 显示行号
 (savehist-mode 1)                            ; （可选）打开 Buffer 历史记录保存
-;(setq display-line-numbers-type 'relative)   ; （可选）显示相对行号
-;autosave in one dir
+;(setq display-line-numbers-type 'relative)  ; （可选）显示相对行号
+(setq-default cursor-type 'bar)              ;设置光标为竖线
+
+					;autosave in one dir
 (setq backup-directory-alist
           `(("." . ,(concat user-emacs-directory "backups"))))
 
@@ -64,6 +66,7 @@
  )
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'process-coding-system-alist '("rg" utf-8 . gbk));;解决counsel-rg无法搜索中文的问题
+(require 'init-dired)
 (require 'init-vertico)
 (require 'init-dashboard)
 (require 'init-fonts)
