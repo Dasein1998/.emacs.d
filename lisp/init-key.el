5facf0483dea77ed104108b4a0bbc265cb68a40b
Dasein1998 <dasein1998@gmail.com>
2023-10-15 21:34:28 +0800

    backup

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -33,3 +33,26 @@
         backward-char
         previous-line
         next-line))
+
+;; Key Modifiers
+(cond
+ ((eq system-type 'windows-nt)
+  ;; make PC keyboard's Win key or other to type Super or Hyper
+  ;; (setq w32-pass-lwindow-to-system nil)
+  (setq w32-lwindow-modifier 'super     ; Left Windows key
+        w32-apps-modifier 'hyper)       ; Menu/App key
+  (w32-register-hot-key [s-t]))
+ ((eq window-system 'mac)
+  ;; Compatible with Emacs Mac port
+  (setq mac-option-modifier 'meta
+        mac-command-modifier 'super)
+  (global-set-key [(super a)] #'mark-whole-buffer)
+  (global-set-key [(super v)] #'yank)
+  (global-set-key [(super c)] #'kill-ring-save)
+  (global-set-key [(super s)] #'save-buffer)
+  (global-set-key [(super l)] #'goto-line)
+  (global-set-key [(super w)] #'delete-frame)
+  (global-set-key [(super z)] #'undo)
+  (global-set-key [(super e)] #'dirvish))
+ )
+
