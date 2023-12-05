ee4e2e0b23e73c92a8d9a47ff0e4761f12bae240
Dasein1998 <dasein1998@gmail.com>
2023-12-05 08:13:19 +0800

    uptate

--- lisp/init-avy.el
+++ lisp/init-avy.el
@@ -2,6 +2,8 @@
 
 (use-package ace-pinyin
   :ensure t
+  :after avy
+  :config
   (ace-pinyin-global-mode 1)
   )
 (use-package avy
@@ -18,4 +20,19 @@
 :config
 (global-set-key (kbd "M-o") 'ace-window)
 (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(defvar aw-dispatch-alist
+  '((?x aw-delete-window "Delete Window")
+	(?m aw-swap-window "Swap Windows")
+	(?M aw-move-window "Move Window")
+	(?c aw-copy-window "Copy Window")
+	(?j aw-switch-buffer-in-window "Select Buffer")
+	(?n aw-flip-window)
+	(?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+	(?c aw-split-window-fair "Split Fair Window")
+	(?v aw-split-window-vert "Split Vert Window")
+	(?b aw-split-window-horz "Split Horz Window")
+	(?o delete-other-windows "Delete Other Windows")
+	(?? aw-show-dispatch-help))
+  "List of actions for `aw-dispatch-default'.")
+
 )
