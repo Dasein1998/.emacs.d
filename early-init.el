cac00d749cd1a3e55984d1184ccac9bf07de2bc2
Dasein1998 <dasein1998@gmail.com>
2024-06-15 06:33:28 +0800

    update early-init

--- early-init.el
+++ early-init.el
@@ -6,5 +6,16 @@
       )
 (tool-bar-mode -1)                           ; 关闭工具栏，tool-bar-mode 即为一个 Minor Mode
 (setq-default mode-line-format nil)          ; Prevent flashing of unstyled modeline at startup
+;; Inhibit resizing Puremacs frame
+(setq frame-inhibit-implied-resize t)
+
+;; To suppress flashing at startup
+(setq-default inhibit-redisplay t
+              inhibit-message t)
+(add-hook 'window-setup-hook
+          (lambda ()
+            (setq-default inhibit-redisplay nil
+                          inhibit-message nil)
+            (redisplay)))
 ;;(setq frame-inhibit-implied-resize t)        ;禁止frame缩放
 (setq native-comp-jit-compilation nil)
